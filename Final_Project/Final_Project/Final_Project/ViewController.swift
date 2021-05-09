ef0a67ce76aad08ae15599903773c728b12dd1e0
Ryan Romero <32284933+rromero26@users.noreply.github.com>
2021-05-09 11:23:03 -0700

    Add files via upload

--- Final_Project/Final_Project/Final_Project/ViewController.swift
+++ Final_Project/Final_Project/Final_Project/ViewController.swift
@@ -11,27 +11,27 @@ class ViewController: UIViewController {
     @IBOutlet var button1 : UIButton!
     @IBOutlet var button2 : UIButton!
     @IBOutlet var display : UIStackView!
+    @IBOutlet var submit : UIButton!
+    @IBOutlet var userText : UITextField!
+    
     var password : String!
     var num_labels : Int!
     
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         // initialize the number of labels
         num_labels = 0
 
     }
     func randomString(length: Int) -> String {
-        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$"
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&+-?<>=^@_;:(){}"
         return String((0..<length).map{ _ in letters.randomElement()!})
     }
     
     
-    func create_label(k : Int){
+    func create_label_random(k : Int){
         let label = UILabel()
         password = randomString(length:k)
         label.text = password
@@ -39,9 +39,22 @@ class ViewController: UIViewController {
         num_labels += 1
     }
     
-    @IBAction func pressed(sender: UIButton!){
-        if(sender == button1){create_label(k: 12)}
-        if(sender == button2){create_label(k: 16)}
+    func create_label_user(k : String) {
+        let label = UILabel()
+        password = k
+        label.text = password
+        display.addArrangedSubview(label)
+        num_labels += 1
+    }
+    
+    @IBAction func password_random(sender: UIButton!){
+        if(sender == button1){create_label_random(k: 12)}
+        if(sender == button2){create_label_random(k: 16)}
+    }
+    
+    @IBAction func password_user(sender: UIButton!) {
+        let text: String = userText.text!
+        if(sender == submit){create_label_user(k: text)}
     }
     
 }
